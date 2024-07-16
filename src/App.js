453bdba29d2ab7821d1eb908549b86f68ffae1f9
mahaboob-basha-a <alurumahaboobbasha@gmail.com>
2024-07-16 09:38:55 +0530

    root directory updat

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
       <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/' element={<Dashboard />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
     </>
